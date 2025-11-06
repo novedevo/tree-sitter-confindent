c4529518d10a33d655927ff94bf0a46c08d7e5c7
Devon Sawatsky <devon@nove.dev>
2025-11-06 04:15:11 +0000

    flake init

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }:   
+  let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+        tree-sitter clang nodejs
+        #rustc cargo clippy rustfmt inetutils python3
+      ];
+      #RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    };
+  };
+}
