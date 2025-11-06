4f158f456e877298ff078fbf18f7378c4c9a3cc6
Devon Sawatsky <devon@nove.dev>
2025-11-06 04:34:05 +0000

    more setup

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     in {
     devShells.x86_64-linux.default = pkgs.mkShell {
       packages = with pkgs; [
-        tree-sitter clang nodejs
+        tree-sitter clang nodejs python3
         #rustc cargo clippy rustfmt inetutils python3
       ];
       #RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
